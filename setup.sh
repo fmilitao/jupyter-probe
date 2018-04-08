0872902ca7a9077780b59a4676d84502e2f4e2f2
Filipe Militão <fm2097ad@gmail.com>
2018-04-08 16:38:27 +0100

    Add tabulate and default to show line numbers

--- setup.sh
+++ setup.sh
@@ -13,14 +13,17 @@ source ${VENV_DIR}/bin/activate
 
 # The alternative would be to use: pip install -r requirements.txt
 # with all requirements placed in that file. Since currently we only have one, not point in doing that.
-pip3 install jupyter
+pip3 install jupyter tabulate
 
 # Needed to make sure `jupyter` command is available
 source ${VENV_DIR}/bin/activate
 
-# FIXME: or try the other?
 # https://stackoverflow.com/questions/39324039/highlight-typos-in-the-jupyter-notebook-markdown
 # For spellchecker in the notebook
 pip3 install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
 jupyter nbextension enable spellchecker/main
+
+# show line numbers by default
+# https://stackoverflow.com/questions/20197471/how-to-display-line-numbers-in-ipython-notebook-code-cell-by-default
+echo 'IPython.Cell.options_default.cm_config.lineNumbers = true;' >> $(find . -iname "custom.js" | grep -v test)
